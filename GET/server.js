1708718b6f646b7abc09cae1d63b697783a6de32
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-06-28 09:11:31 -0700

    completed GET assignment

--- GET/server.js
+++ GET/server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -17,11 +18,17 @@ app.use(morgan('common'));
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
+Recipes.create('Chicken Pot Pie', ['chicken', 'dough', 'vegetables']);
+Recipes.create('BLT', ['bacon', 'lettuce', 'tomato', 'bread']);
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
